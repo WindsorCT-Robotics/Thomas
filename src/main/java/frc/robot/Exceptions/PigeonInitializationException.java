35996df75bb41d6d152fada8cb86ad4eddf9e0f6
Charles Christie <sonicbhoc@gmail.com>
2023-02-05 23:12:19 -0500

    Add exception type and timeout to NineAxis

---
+++ src/main/java/frc/robot/Exceptions/PigeonInitializationException.java
@@ -0,0 +1,9 @@
+package frc.robot.Exceptions;
+
+import com.ctre.phoenix.ErrorCode;
+
+public class PigeonInitializationException extends RuntimeException {
+    public PigeonInitializationException (ErrorCode error) {
+        super(String.format("Failed to initialize Pigeon IMU: %d", error.value));
+    }
+}
--- src/main/java/frc/robot/subsystems/NineAxis.java
+++ src/main/java/frc/robot/subsystems/NineAxis.java
@@ -3,18 +3,26 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.sensors.WPI_Pigeon2;
 
-import frc.robot.Mathematics.Degrees;
+import frc.robot.Exceptions.PigeonInitializationException;
+import frc.robot.Types.Degrees;
+import frc.robot.Types.Milliseconds;
 
 public class NineAxis {
     private final WPI_Pigeon2 pidgey;
 
-    public NineAxis(WPI_Pigeon2 pigeon2) throws Exception {
+    public NineAxis(WPI_Pigeon2 pigeon2, Milliseconds timeout) throws PigeonInitializationException {
         pidgey = pigeon2;
         pidgey.reset();
 
-        if (pidgey.configFactoryDefault() != ErrorCode.OK) {
-            throw new Exception("Error initializing Pigeon2");
+        ErrorCode result = pidgey.configFactoryDefault(timeout.getMilliseconds());
+
+        if (result != ErrorCode.OK) {
+            throw new PigeonInitializationException(result);
         }
+    }
+
+    public NineAxis(WPI_Pigeon2 pigeon2) throws PigeonInitializationException {
+        this(pigeon2, new Milliseconds(0));
     }
 
     public void reset() {
